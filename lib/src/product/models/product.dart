afed0980ed8227290d011e02e0ec46374b2028e9
dev <d.loziuk@spartacus-mma.com>
2024-11-27 11:02:25 +0200

    daySchedules

--- lib/src/product/models/product.dart
+++ lib/src/product/models/product.dart
@@ -46,6 +46,25 @@ enum DayOfWeek {
         throw ArgumentError('Day is undefined => $day');
     }
   }
+
+  int get weekday {
+    switch (this) {
+      case DayOfWeek.monday:
+        return 1;
+      case DayOfWeek.tuesday:
+        return 2;
+      case DayOfWeek.wednesday:
+        return 3;
+      case DayOfWeek.thursday:
+        return 4;
+      case DayOfWeek.friday:
+        return 5;
+      case DayOfWeek.saturday:
+        return 6;
+      case DayOfWeek.sunday:
+        return 7;
+    }
+  }
 }
 
 class WooProduct {
